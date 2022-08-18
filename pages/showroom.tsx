649c0ff30246a35860b59096e656fd2137c332fe
Arda Sevinç <ardasevinc.technical@gmail.com>
2022-08-19 02:43:43 +0300

    added a link to go back to index

--- pages/showroom.tsx
+++ pages/showroom.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Card from "../components/card";
 
 const Showroom = () => {
@@ -13,7 +14,12 @@ const Showroom = () => {
   return (
     <div className="flex-1 bg-slate-900 text-white">
       <header className="p-8">
-        <h1 className="text-4xl font-bold">The Showroom</h1>
+        <h1 className="text-4xl font-bold mb-4">The Showroom</h1>
+        <Link href="/">
+          <a className="text-lg hover:ring hover:ring-sky-400 underline rounded-md p-2 focus:outline-none focus-visible:ring focus-visible:ring-sky-400">
+            Go back
+          </a>
+        </Link>
       </header>
       <main className="mx-auto max-w-4xl p-4 outline-dashed outline-2">
         <div className="flex flex-col gap-y-8">
